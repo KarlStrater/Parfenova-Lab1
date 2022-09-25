85ff5e8774d1cd7f275c3aba67fb182e53afb6e1
Arina Parfenova <78087757+KarlStrater@users.noreply.github.com>
2022-09-25 14:57:38 +0400

    Add files via upload

---
+++ N strings/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("N stars");
+int n;
+bool isNumber = false;
+string stars = "*";
+while (!isNumber)
+{
+    Console.WriteLine("Write the number:");
+    string? line = Console.ReadLine();
+    isNumber = int.TryParse(line, out n);
+    if (!isNumber)
+    {
+        Console.WriteLine("Wrong input... Try again)");
+    }
+    else if(n<=0)
+    { Console.WriteLine("Wrong input... Try again)"); }    
+    for(int k=1; k<=n; k++)
+    {
+        Console.WriteLine(stars);
+        stars += "*";
+    }
+}
+
+
