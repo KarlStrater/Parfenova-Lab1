5df2c2fabe8c4afd8c65baf6d78a835107a90e44
Arina Parfenova <78087757+KarlStrater@users.noreply.github.com>
2022-10-21 14:26:38 +0400

    Add files via upload

---
+++ Lab_one/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿
+byte ByteTry()
+{
+    byte res = 0;
+    bool trying = true;
+    {
+        while (trying)
+        {
+            bool sucsess = byte.TryParse(Console.ReadLine(), out res);
+            if (!sucsess)
+            {
+                Console.WriteLine("Write the number");
+            }
+            else
+            {
+                if (res < 0)
+                {
+                    Console.WriteLine("Wrong imput");
+                }
+                else
+                {
+                    trying = false;
+                }
+            }
+        }
+        return res;
+    }
+}
+Console.WriteLine("Write the first number:");
+byte first = ByteTry();
+Console.WriteLine("Write the second number:");
+byte second = ByteTry();
+Console.WriteLine("&");
+Console.WriteLine(first & second);
+Console.WriteLine("|");
+Console.WriteLine(first | second);
+Console.WriteLine("^");
+Console.WriteLine(first ^ second);
